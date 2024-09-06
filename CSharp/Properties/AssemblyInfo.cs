99f1dc893a9a917e2863a5a066879ebb7298ea9f
vintasoft <alex@vintasoft.com>
2024-09-06 14:33:17 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("12.5.2.1")]
-[assembly: AssemblyFileVersion("12.5.2.1")]
+[assembly: AssemblyVersion("12.5.3.1")]
+[assembly: AssemblyFileVersion("12.5.3.1")]
